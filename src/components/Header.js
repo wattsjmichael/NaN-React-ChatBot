c05d6ae7cddca793c096343b88bb5af5a1ef351d
Michael Watts <wattsjmichael@gmail.com>
2020-12-18 15:58:29 -0800

    Adds Header Image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import headerPixelBar from "./../img/headerPixelBar.jpg";
 
 function Header() {
   return(
     <div style={{ backgroundColor: 'black', color:'whitesmoke' }}>
+      <img src={headerPixelBar} alt="Picture of a bar from the future" class="responsive"></img>
       <h1>NaN</h1>
       <h3>The Brewery of the future</h3>
     </div>
