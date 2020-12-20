fb9b6f646ed0f76db21759af71ce1bbbf613ea29
Michael Watts <wattsjmichael@gmail.com>
2020-12-19 16:28:33 -0800

    Creates a new form 'Needs to be available only for employees'

--- src/components/NewBeerForm.js
+++ src/components/NewBeerForm.js
@@ -1,12 +1,36 @@
 //Need to be authorized to be able to access this page // 
-
+import { v4 } from 'uuid';
 
 import React from 'react';
 
 function NewBeerForm(props){
+
+  function handleNewBeerFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.style.value);
+    console.log(event.target.price.value);
+  }
+
+
+
   return (
     <React.Fragment>
-      <h1>New Beers go here!</h1>
+      <form onSubmit={handleNewBeerFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name' />
+        <input
+          type='text'
+          name='style'
+          placeholder='Style' />
+        <input
+          type= 'text'
+          name= 'price'
+          placeholder='Enter Price' />
+        <button type='submit'>Enter</button>
+      </form>
     </React.Fragment>
   );
 }
