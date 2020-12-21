751b0ba83c786815850fc57f7bd62766c3bf67e3
Michael Watts <wattsjmichael@gmail.com>
2020-12-21 12:39:08 -0800

    CHATBOT CONNECTS!

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import BeerControl from "./BeerControl";
+import LexChat from 'react-lex';
+
 
 
 function App() {
@@ -9,6 +11,14 @@ function App() {
     <React.Fragment>
       <Header />
       <BeerControl />
+      <LexChat botName="OrderFlowers"
+        IdentityPoolId="us-east-1:7292b8c0-56f1-4441-b2a6-xxxxxxxxxxxx"
+        placeholder="Placeholder text"
+        style={{ position: 'absolute' }}
+        backgroundColor="#FFFFFF"
+        height="430px"
+        headerText="Chat with our awesome bot" />
+
     </React.Fragment>
   );
 }
