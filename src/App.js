3bba9cfaefc6ba10bf503cf6867c593931d25f28
Michael Watts <wattsjmichael@gmail.com>
2020-12-22 11:39:34 -0800

    working on another Project to see if I can combine the two

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import { SumerianScene } from 'aws-amplify-react';
+import Amplify from 'aws-amplify';
+import Aws_exports from './aws-exports';
+import '@aws-amplify/ui/dist/style.css';
+
+Amplify.configure(Aws_exports);
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{ height: '100vh' }}>
+        <SumerianScene sceneName='scene1' />
+      </div>
+    );
+  }
+};
+
+export default App;
